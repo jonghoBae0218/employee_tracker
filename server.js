1b992fc034f4d30064544e35c4c76a634f7bdf8a
Jongho Bae <jb4918@columbia.edu>
2024-07-05 21:19:36 -0400

    Initial setup + schema.sql setup

---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require('express');
+// Import and require Pool (node-postgres)
+// We'll be creating a Connection Pool. Read up on the benefits here: https://node-postgres.com/features/pooling
+const { Pool } = require('pg');
+
+const PORT = process.env.PORT || 3001;
+const app = express();
+
+
+
+
+
+
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+  });
+  
